9f14f71097c6474d3f15215f6f676ef5804631c2
ltg_17710666165 <ltg263@126.com>
2018-04-23 20:50:49 +0800

    Http demo

--- app/src/main/java/com/secretk/move/ui/activity/HttpActivity.java
+++ app/src/main/java/com/secretk/move/ui/activity/HttpActivity.java
@@ -3,16 +3,28 @@ package com.secretk.move.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.google.gson.Gson;
 import com.secretk.move.R;
+import com.secretk.move.apiService.HttpCallBackImpl;
+import com.secretk.move.apiService.RetrofitUtil;
+import com.secretk.move.apiService.RxHttpParams;
+import com.secretk.move.baseManager.Constants;
+import com.secretk.move.bean.DynamicValidateCodeSend;
 import com.secretk.move.bean.postbean.PostRegisterBean;
 import com.secretk.move.bean.returnbean.RegisterBean;
 import com.secretk.move.http.Network;
 import com.secretk.move.utils.AESUtil;
 import com.secretk.move.utils.CrashHandler;
+import com.secretk.move.utils.LogUtil;
+import com.secretk.move.utils.MD5;
+import com.secretk.move.utils.PolicyUtil;
 import com.secretk.move.utils.ToastUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
@@ -37,7 +49,7 @@ public class HttpActivity extends AppCompatActivity {
             @Override
             public void accept(RegisterBean versionBean) throws Exception {
               String str="PostRegisterBean";
-                ToastUtils.getInstance().show(str);
+                ToastUtils.getInstance().show(versionBean.msg);
             }
         }, new Consumer<Throwable>() {
             @Override
@@ -50,6 +62,26 @@ public class HttpActivity extends AppCompatActivity {
         });
     }
     public void rxGetVerificationCode(View view){
-
+        //String str = "{\"phone\":\"15967158669\",\"module\":\"register\"}";
+        JSONObject node = new JSONObject();
+        try {
+            node.put("phone", "15967158669");
+            node.put("module", "register");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RxHttpParams params = new RxHttpParams.Build()
+                .url(Constants.DYNAMIC_VALIDATE_CODE_SEND)
+                .addQuery("policy", PolicyUtil.encryptPolicy(node.toString()))
+                .addQuery("sign", MD5.Md5(node.toString()))
+                .build();
+        //网络请求方式 默认为POST
+        RetrofitUtil.request(params, DynamicValidateCodeSend.class, new HttpCallBackImpl<DynamicValidateCodeSend>() {
+            @Override
+            public void onCompleted(DynamicValidateCodeSend contactsBean) {
+                LogUtil.w(contactsBean.getMsg());
+                ToastUtils.getInstance().show(contactsBean.getData().getDynamicCode());
+            }
+        });
     }
 }
